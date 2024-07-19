a2f5fb6958965d12cf825afa784fe29935efe1e1
Aidan Snyder <ASnyder380@myfcc.frederick.edu>
2024-07-19 15:22:42 -0400

    Included Launcher.java for Jar file.

---
+++ src/Launcher.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+
+/**
+ * This is the main class that is used when building the project into the Jar file.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(PaintApplication.class, args);
+    }
+}
